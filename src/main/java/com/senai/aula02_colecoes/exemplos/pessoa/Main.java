9d82d2cc06b6f17ef289193f69a7519876afc522
brayanxd4547 <brayanlainescarvalho@gmail.com>
2025-02-14 11:02:37 -0300

    Aula02 - continuação

--- src/main/java/com/senai/aula02_colecoes/exemplos/pessoa/Main.java
+++ src/main/java/com/senai/aula02_colecoes/exemplos/pessoa/Main.java
@@ -2,9 +2,14 @@ package com.senai.aula02_colecoes.exemplos.pessoa;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         Pessoa pessoa1 = new Pessoa("Umberto", 10);
         Pessoa pessoa2 = new Pessoa("Doisberto", 20);
         Pessoa pessoa3 = new Pessoa("Tresberto", 30);
@@ -19,44 +24,70 @@ public class Main {
             System.out.println(pessoa);
         }
 
-        matrizPessoas[0].falar("Alô galera de cowboy");
+        matrizPessoas[0].falar("Convocarei o quarto integrante da família.");
 
         matrizPessoas[3] = new Pessoa("Quatroberto", 40);
         System.out.println(matrizPessoas[3].nome);
 
+        // Criando um ArrayList (lista)
         ArrayList lista = new ArrayList();
 
+        // add: adiciona elementos (com ou sem índice) na lista (tamanho variável)
         lista.add(3);
         lista.add(true);
         lista.add("ygiy");
         lista.add(pessoa1);
 
+        // get: retorna o elemento da lista com o índice escolhido
         Pessoa pessoa = (Pessoa) lista.get(3);
-        pessoa.falar("Alô");
+        pessoa.falar("Todos os membros estão convocados.\n");
 
+        // <diamond>: limita os tipos de elemento por tipo de objeto
         ArrayList<Pessoa> listaPessoas = new ArrayList();
 
-        listaPessoas.add(pessoa1);
+        listaPessoas.add(matrizPessoas[0]);
+        listaPessoas.add(matrizPessoas[1]);
+        listaPessoas.add(matrizPessoas[2]);
+        listaPessoas.add(matrizPessoas[3]);
         listaPessoas.add(new Pessoa("Cincoberto", 50));
 
-        System.out.println("size: " + listaPessoas.size());
+        System.out.println("Tamanho da lista: " + listaPessoas.size());
 
-        listar(listaPessoas);
+        System.out.println("\n------------ Lista ------------"); // forEach: executa um comando para cada elemento na lista
+        listaPessoas.forEach(System.out::println);
 
-        listaPessoas.remove(pessoa1);
-        listar(listaPessoas);
+        System.out.println("\n------------ Lista ------------");
+        listaPessoas.remove(pessoa1); // remove: remove um elemento da lista (pelo índice ou pela referência)
+        listaPessoas.forEach(System.out::println);
 
-        listaPessoas.set(0, pessoa2);
-        listar(listaPessoas);
+        System.out.println("\n------------ Lista ------------");
+        listaPessoas.set(0, pessoa1); // set: substitui um elemento da lista por outro
+        listaPessoas.forEach(System.out::println);
 
-        listaPessoas.add(0, pessoa3);
-        listar(listaPessoas);
-    }
+        System.out.println("\n------------ Lista ------------");
+        listaPessoas.add(0, pessoa2);
+        listaPessoas.forEach(System.out::println);
 
-    public static void listar(ArrayList<Pessoa> lista) {
-        for (Pessoa pessoa : lista){
-            System.out.println(pessoa);
-        }
-        System.out.println();
+        System.out.println("\nFiltrando para apenas as pessoas com idade igual a 30 ou maior:");
+        // stream: executa certas funções num fluxo de cada elemento da lista
+        // filter: filtra apenas os elementos que retornam "true" a uma condição
+        // toList: reagrupa os elementos em uma lista
+        listaPessoas.stream().filter(p -> p.idade >= 30).toList().forEach(System.out::println);
+
+        System.out.println("\nFiltrando para apenas as pessoas com 'ober' no nome:");
+        listaPessoas.stream().filter(p -> p.nome.toLowerCase().contains("ober")).toList().forEach(System.out::println);
+
+        System.out.println("Preencha as informações a seguir: ");
+        System.out.print("Nome: ");
+        String nome = scanner.nextLine();
+        System.out.print("Idade: ");
+        int idade = scanner.nextInt();
+        scanner.nextLine();
+
+        listaPessoas.add(new Pessoa(nome, idade));
+        System.out.println("\n------------ Lista ------------");
+        listaPessoas.forEach(System.out::println);
+
+        scanner.close();
     }
 }
